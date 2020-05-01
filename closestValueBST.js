9b8f4904410c9ec1de9c9a9c3e1489c330735a03
Eric P <56278025+CodeYut@users.noreply.github.com>
2020-05-01 06:17:56 -0700

    Create closestValueBST.js

---
+++ closestValueBST.js
@@ -0,0 +1,20 @@
+function findClosestValueInBst(tree, target) {
+  // Write your code here.
+	return findClosestValueInBstHelper(tree, target, Infinity);
+}
+
+
+function findClosestValueInBstHelper(tree, target, closest){
+	if (tree === null) return closest;
+	if (Math.abs(target - closest) > Math.abs(target - tree.value)){
+		closest = tree.value;
+	}
+
+if (target < tree.value){
+	return findClosestValueInBstHelper(tree.left, target, closest);
+}else if (target > tree.value){
+	return findClosestValueInBstHelper(tree.right, target, closest);
+}else {
+	return closest;
+}
+}
